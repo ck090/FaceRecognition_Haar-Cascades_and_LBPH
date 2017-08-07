db4c1087dc80f53a316c8c5cc4688be4ef79b5d5
N.Chandra Kanth <chandrakanth@Chandras-MacBook-Pro.local>
2017-08-07 21:29:08 +0530

    N

--- Pydata1.py
+++ Pydata1.py
@@ -5,17 +5,18 @@ from matplotlib import pyplot as plt
 import cv2
 
 webcam = cv2.VideoCapture(0)
+cv2.namedWindow("Live Feed", cv2.WINDOW_NORMAL)
+cv2.resizeWindow("Live  Feed", 300, 400)
 
-while(1):
-
+while 1:
     ret, frame = webcam.read()
-    print ret
 
-    cv2.startWindowThread()
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    cv2.putText(frame, 'Press \'q\' to exit', (10,20), font, 0.5, (0,0,0), 1, cv2.LINE_AA)
+    cv2.imshow("Live Feed", frame)
 
-    #cv2.namedWindow("First", cv2.WINDOW_NORMAL)
-    cv2.imshow("TUTS", frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
-cv2.waitKey()
 cv2.destroyAllWindows()
 webcam.release()
